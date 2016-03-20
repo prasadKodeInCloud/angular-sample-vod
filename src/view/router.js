0843bfae088f95eeabe474fa2bd055f7067d8065
PrasadKodeincloud <Prasad19sara@gmail.com>
2016-03-21 02:17:55 +0800

    added html view

--- src/view/router.js
+++ src/view/router.js
@@ -1,14 +1,14 @@
 
 
 function setup( app,handlers) {
+	app.get('/', function(req, res){
+ 		res.sendfile('./public/app/index.html'); 
+	});
+
 	console.log("set router ####");
 	app.get('/api/user', handlers.user.get );
 	app.post('/api/users', handlers.user.create );
 	app.post('/api/find', handlers.user.find );	
-
-	app.get('/', function(req, res){
- 		res.send('<h1>Hello, welcome to VOD');
-	});
 }
  
 exports.setup = setup;
