2ae566d6af4947dfad215f2ca2ceaff5d9dab92d
PrasadKodeincloud <Prasad19sara@gmail.com>
2016-03-22 02:58:13 +0800

    added cintroller

---
+++ public/app/src/controller/video.ctrl.js
@@ -0,0 +1,15 @@
+
+
+app.controller('videoController', ['$scope', function( $scope ){
+	
+	$scope.params = {};
+
+	$scope.init = function(){
+		$scope.showVideoBar();
+	}
+
+	$scope.showVideoBar = function(){
+
+	}
+
+}]);
